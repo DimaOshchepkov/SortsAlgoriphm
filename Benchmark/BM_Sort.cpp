a3c2be9a43fe544a19d30d430a0e3ff16bacf364
DimaOshchepkov <odo.2003@yandex.ru>
2024-05-20 17:01:14 +0300

    Сделал замечания

--- Benchmark/BM_Sort.cpp
+++ Benchmark/BM_Sort.cpp
@@ -11,6 +11,7 @@
 #include "../SortsAlgoriphm/MergeSort.h"
 #include "../SortsAlgoriphm/SupportFunction.h"
 #include "../SortsAlgoriphm/CountingSort.h"
+#include "../SortsAlgoriphm/TimSort.h"
 
 using namespace std;
 
@@ -81,70 +82,58 @@ static void BM_CountingSort(benchmark::State& state, const std::string& arrayFil
 }
 
 
+static void BM_TimSort(benchmark::State& state, const std::string& arrayFileName) {
+    auto fullPath = getSolutionDir() + "\\" + arrayFileName;
+    std::vector<int> array = readIntegersFromFile(fullPath);
+
+    for (auto _ : state) {
+        auto arr = timSort<int>(array);
+        benchmark::DoNotOptimize(arr);
+    }
+}
+
+
 int main(int argc, char** argv) {
 
-    for (int i = 1'000'000; i <= 20'000'000; i += 1'000'000) {
-
-        auto intNormalFileName = std::format("arrays\\int_normal_{}.txt", i);
-        auto intNormalName = std::format("int_normal_{}.txt", i);
-        benchmark::RegisterBenchmark("BM_MergeSort/" + intNormalName, BM_MergeSort, intNormalFileName)->Unit(benchmark::kMillisecond);
-        benchmark::RegisterBenchmark("BM_CountingSort/" + intNormalName, BM_CountingSort, intNormalFileName)->Unit(benchmark::kMillisecond);
-
+    for (int i = 5'000'000; i <= 41'000'000; i += 3'000'000) {
         auto intUniformFileName = std::format("arrays\\int_uniform_{}.txt", i);
         auto intUniformName = std::format("int_uniform_{}.txt.txt", i);
         benchmark::RegisterBenchmark("BM_MergeSort/" + intUniformName, BM_MergeSort, intUniformFileName)->Unit(benchmark::kMillisecond);
-       
+        benchmark::RegisterBenchmark("BM_TimSort/" + intUniformName, BM_TimSort, intUniformFileName)->Unit(benchmark::kMillisecond);
         benchmark::RegisterBenchmark("BM_CountingSort/" + intUniformName, BM_CountingSort, intUniformFileName)->Unit(benchmark::kMillisecond);
     }
-    /*
-    
-    for (int i = 10'000; i <= 300'000; i += 10'000) {
-        auto intNormalFileName = std::format("arrays\\int_normal_{}.txt", i);
-        auto intNormalName = std::format("int_normal_{}.txt", i);
-        benchmark::RegisterBenchmark("BM_MergeSort/" + intNormalName, BM_MergeSort, intNormalFileName)->Unit(benchmark::kMillisecond);
-        benchmark::RegisterBenchmark("BM_InsertionSort/" + intNormalName, BM_InsertionSort, intNormalFileName)->Unit(benchmark::kMillisecond);
-        benchmark::RegisterBenchmark("BM_BubbleSort/" + intNormalName, BM_BubbleSort, intNormalFileName)->Unit(benchmark::kMillisecond);
-        benchmark::RegisterBenchmark("BM_CountingSort/" + intNormalName, BM_CountingSort, intNormalFileName)->Unit(benchmark::kMillisecond);
 
+    for (int i = 20'000; i <= 300'000; i += 20'000) {
         auto intUniformFileName = std::format("arrays\\int_uniform_{}.txt", i);
-        auto intUniformName = std::format("int_uniform_{}.txt", i);
+        auto intUniformName = std::format("int_uniform_{}.txt.txt", i);
         benchmark::RegisterBenchmark("BM_MergeSort/" + intUniformName, BM_MergeSort, intUniformFileName)->Unit(benchmark::kMillisecond);
+        benchmark::RegisterBenchmark("BM_CountingSort/" + intUniformName, BM_CountingSort, intUniformFileName)->Unit(benchmark::kMillisecond);
         benchmark::RegisterBenchmark("BM_InsertionSort/" + intUniformName, BM_InsertionSort, intUniformFileName)->Unit(benchmark::kMillisecond);
         benchmark::RegisterBenchmark("BM_BubbleSort/" + intUniformName, BM_BubbleSort, intUniformFileName)->Unit(benchmark::kMillisecond);
-        benchmark::RegisterBenchmark("BM_CountingSort/" + intUniformName, BM_CountingSort, intUniformFileName)->Unit(benchmark::kMillisecond);
-    }*/
-    /*
-    for (int i = 500; i <= 10'000; i += 500) {
-        auto intNormalFileName = std::format("arrays\\int_normal_{}.txt", i);
-        auto intNormalName = std::format("int_normal_{}.txt", i);
-        benchmark::RegisterBenchmark("BM_MergeSort/" + intNormalName, BM_MergeSort, intNormalFileName)
-            ->Unit(benchmark::kMillisecond)
-            ->Iterations(100);
-        
+        benchmark::RegisterBenchmark("BM_TimSort/" + intUniformName, BM_TimSort, intUniformFileName)->Unit(benchmark::kMillisecond);
+    }
 
+    for (int i = 1'000; i <= 10'000; i += 1'000) {
         auto intUniformFileName = std::format("arrays\\int_uniform_{}.txt", i);
-        auto intUniformName = std::format("int_uniform_{}.txt", i);
-        benchmark::RegisterBenchmark("BM_MergeSort/" + intUniformName, BM_MergeSort, intUniformFileName)
-            ->Unit(benchmark::kMillisecond)
-            ->Iterations(100);
-     
+        auto intUniformName = std::format("int_uniform_{}.txt.txt", i);
+        benchmark::RegisterBenchmark("BM_MergeSort/" + intUniformName, BM_MergeSort, intUniformFileName)->Unit(benchmark::kMillisecond);
+        benchmark::RegisterBenchmark("BM_CountingSort/" + intUniformName, BM_CountingSort, intUniformFileName)->Unit(benchmark::kMillisecond);
+        benchmark::RegisterBenchmark("BM_InsertionSort/" + intUniformName, BM_InsertionSort, intUniformFileName)->Unit(benchmark::kMillisecond);
+        benchmark::RegisterBenchmark("BM_BubbleSort/" + intUniformName, BM_BubbleSort, intUniformFileName)->Unit(benchmark::kMillisecond);
+        benchmark::RegisterBenchmark("BM_TimSort/" + intUniformName, BM_TimSort, intUniformFileName)->Unit(benchmark::kMillisecond);
     }
 
     for (int i = 50; i <= 500; i += 50) {
-        auto intNormalFileName = std::format("arrays\\int_normal_{}.txt", i);
-        auto intNormalName = std::format("int_normal_{}.txt", i);
-        benchmark::RegisterBenchmark("BM_MergeSort/" + intNormalName, BM_MergeSort, intNormalFileName)
-            ->Unit(benchmark::kMillisecond)
-            ->Iterations(100);
-
-
         auto intUniformFileName = std::format("arrays\\int_uniform_{}.txt", i);
-        auto intUniformName = std::format("int_uniform_{}.txt", i);
-        benchmark::RegisterBenchmark("BM_MergeSort/" + intUniformName, BM_MergeSort, intUniformFileName)
-            ->Unit(benchmark::kMillisecond)
-            ->Iterations(100);
+        auto intUniformName = std::format("int_uniform_{}.txt.txt", i);
+        benchmark::RegisterBenchmark("BM_MergeSort/" + intUniformName, BM_MergeSort, intUniformFileName)->Unit(benchmark::kMillisecond);
+        benchmark::RegisterBenchmark("BM_CountingSort/" + intUniformName, BM_CountingSort, intUniformFileName)->Unit(benchmark::kMillisecond);
+        benchmark::RegisterBenchmark("BM_InsertionSort/" + intUniformName, BM_InsertionSort, intUniformFileName)->Unit(benchmark::kMillisecond);
+        benchmark::RegisterBenchmark("BM_BubbleSort/" + intUniformName, BM_BubbleSort, intUniformFileName)->Unit(benchmark::kMillisecond);
+        benchmark::RegisterBenchmark("BM_TimSort/" + intUniformName, BM_TimSort, intUniformFileName)->Unit(benchmark::kMillisecond);
+    }
 
-    }*/
+
 
     benchmark::Initialize(&argc, argv);
     benchmark::RunSpecifiedBenchmarks();
